cf7c780516fe5f3f10f7962b7480b688efac0027
YanivRidel <117529227+Yanivridel@users.noreply.github.com>
2024-10-29 18:46:49 +0200

    deploy for vercel 1

--- api/key.js
+++ api/key.js
@@ -2,7 +2,7 @@
 export default function handler(req, res) {
     const WEATHER_API = process.env.WEATHER_API; 
     if (!WEATHER_API) {
-        return res.status(500).json({ error: 'Bearer token is not set' });
+        return res.status(504).json({ error: 'Weather Key is not set' });
     }
     res.status(200).json({ WEATHER_API });
 }
